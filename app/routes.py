267398ccd83de5b9f20974cbcf92456e828eaade
Eesh Desai <eeshdesai@gmail.com>
2025-01-12 13:15:27 -0800

    created and added basic layout of app and a basic login page.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
 from app import app
 import app.backend.backend as b
+from flask import render_template
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return f"hello there {b.main()}"
+    user = {"username":"Ram"}
+    return render_template("index.html", good="OWIEJFOWEJIEFOWEJI", user=user)
