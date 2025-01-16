684394311483167cd2da385900148d8c60c67ae8
ram-mashruwala <the.ram.mashruwala@gmail.com>
2025-01-15 18:09:18 -0800

    added new login page and refactored template files

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,19 @@
 from app import app
 import app.backend.backend as b
-from flask import render_template
+from flask import render_template, flash, redirect
 import app.backend.User as user
+from app.forms import LoginForm
 
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template("template.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect("/index")
+    return render_template("login.html", title="Ram", form=form)
