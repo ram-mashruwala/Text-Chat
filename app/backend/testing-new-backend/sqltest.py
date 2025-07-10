cf93e3a098371d04ea78c15d55a7a5d9ce19761f
ram-mashruwala <113854904+ram-mashruwala@users.noreply.github.com>
2025-07-09 19:16:24 -0700

    Updated Schema of database to include received messages

--- app/backend/testing-new-backend/sqltest.py
+++ app/backend/testing-new-backend/sqltest.py
@@ -1,56 +1,64 @@
+import sqlalchemy as sa
 from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy.orm as orm
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey, String
 
-Base = orm.declarative_base()
+engine = sa.create_engine("sqlite+pysqlite:///:memory:", echo=True)
+
+class Base(orm.DeclarativeBase): pass
 
 class User(Base):
     __tablename__ = "user"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(String())
-    full_name: Mapped[Optional[str]]
+    username: Mapped[str] = mapped_column(String(30))
+    full_name: Mapped[str]
 
-    posts: Mapped[list["Post"]] = relationship(back_populates="author")
+    sent_messages: Mapped[list["Post"]] = relationship(back_populates="author")
+
+    received_messages: Mapped[list["Post"]] = relationship(back_populates="receiver")
 
     def __repr__(self) -> str:
-        full_name_temp = self.full_name
-        if not full_name_temp:
-            full_name_temp = "unnamed"
-        return f"<User username={self.username!r}, full_name={full_name_temp!r}>"
+        return f"<User id={self.id!r}, username={self.username!r}, full_name={self.full_name!r}>"
 
 
 class Post(Base):
     __tablename__ = "post"
 
+
     id: Mapped[int] = mapped_column(primary_key=True)
     content: Mapped[str]
     time: Mapped[datetime] = mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
-    user_id = mapped_column(ForeignKey("user.id"))
+    sender_id = mapped_column(ForeignKey("user.id"))
+    receiver_id = mapped_column(ForeignKey("user.id"))
 
-    author: Mapped["User"] = relationship(back_populates="posts")
+    author: Mapped["User"] = relationship(back_populates="sent_messages")
+    receiver: Mapped["User"] = relationship(back_populates="received_messages")
 
     def __repr__(self) -> str:
-        return f"<Post content={self.content!r} author={self.author.username!r}>"
+        return f"<Post id= {self.id!r}, content={self.content!r} author={self.author.username!r}>"
 
 
 
 def main():
-    test_user1 = User(username="test_user", full_name="Test User")
-    test_user2 = User(username="second_test_user")
-    test_post1 = Post(content="This is a Test Post", author=test_user1)
-    test_post2 = Post(content="This is another Test Post", author=test_user1)
-    test_post3 = Post(content="This is the third Test Post", author=test_user2)
+    Base.metadata.create_all(engine)
+    # test_user1 = User(username="test_user", full_name="Test User")
+    # test_user2 = User(username="second_test_user")
+    # test_post1 = Post(content="This is a Test Post", author=test_user1)
+    # test_post2 = Post(content="This is another Test Post", author=test_user1)
+    # test_post3 = Post(content="This is the third Test Post", author=test_user2)
+    #
+    # print(test_user1)
+    # print(test_user2)
+    # print(test_post1)
+    # print(test_post2)
+    # print(test_post3, "\n")
+    # print(test_user1.posts)
+    # print(test_user2.posts)
+    # print(sa.select(User.posts))
 
-    print(test_user1)
-    print(test_user2)
-    print(test_post1)
-    print(test_post2)
-    print(test_post3, "\n")
-    print(test_user1.posts)
-    print(test_user2.posts)
 
 
 if __name__ == "__main__":
