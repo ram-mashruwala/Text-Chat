de4ac64ba86c233d5b5e04340abe868a4a36c34e
ram-mashruwala <113854904+ram-mashruwala@users.noreply.github.com>
2025-07-22 10:36:44 -0700

    Added new Form type for registration

--- app/backend/forms.py
+++ app/backend/forms.py
@@ -6,3 +6,13 @@ class LoginForm(FlaskForm):
     username = StringField(label="Username", validators=[DataRequired()])
     password = PasswordField(label="Password", validators=[DataRequired()])
     submit = SubmitField(label="Sign In")
+
+class RegisterForm(FlaskForm):
+    username = StringField(label="Username", validators=[DataRequired()])
+    # We don't need this, but we have it just to look professional
+    email = StringField(label="Email", validators=[DataRequired()])
+    phoneNumber = StringField(label="phoneNumber", validators=[DataRequired()])
+    # We need the rest
+    password = StringField(label="Password", validators=[DataRequired()])
+    resubmit_password = StringField(label="resubmit_password", validators=[DataRequired()])
+    submit = SubmitField(label="Register")
