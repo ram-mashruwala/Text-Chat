d1da63e0fcfe1193189b57873042e04e320afbf2
ram-mashruwala <113854904+ram-mashruwala@users.noreply.github.com>
2025-07-23 12:38:12 -0700

    Fixed multiple bugs and updated database
    First bug fixed: backpopulates on the Chats table for users was wrong,
    it was "chat" instead of chats
    Second bug: Added chat_id to messages table to fix bug of sqlalchemy not
    being able to link chats table and messages table
    
    Updates to all tables in database: Added names for all database tables
    and added a __repr__ functin for all tables in database
    
    Updated User table in database: Made password_hash optional to encourage
    the use of "set_password" function. Made email forced to be unique. Made
    chats optional since a user can be in no chats
    
    Updated Chats table in database: Added cascade parameter to relationship
    function to have better deletion
    
    Updated Messages table in database: Included text, time, and author with
    a one way relationship

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
+from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-from typing import List
+from typing import List, Optional
 from app import db
 from sqlalchemy import Column, ForeignKey, Table, String
 from sqlalchemy.orm import relationship, Mapped, mapped_column
@@ -11,23 +12,47 @@ association_table = Table(
 )
 
 class User(db.Model):
+    __tablename__ = "user"
+
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(String(30), index=True, unique=True)
-    password_hash: Mapped[str] = mapped_column(String(256))
-    email: Mapped[str] = mapped_column(index=True)
-    chats: Mapped[List["Chats"]] = relationship(secondary=association_table, back_populates="users")
+    password_hash: Mapped[Optional[str]] = mapped_column(String(256))
+    email: Mapped[str] = mapped_column(String(120), index=True, unique=True)
+    chats: Mapped[Optional[List["Chats"]]] = relationship(secondary=association_table, back_populates="users")
 
     def set_password(self, password: str) -> None:
         self.password_hash = generate_password_hash(password=password)
 
     def check_password(self, password: str) -> bool:
+        if not self.password_hash:
+            return False
         return check_password_hash(self.password_hash, password=password)
 
+    def __repr__(self) -> str:
+        return f"<User id={self.id} username={self.username} email={self.email}>"
+
 class Chats(db.Model):
+    __tablename__ = "chats"
+
     id: Mapped[int] = mapped_column(primary_key=True)
-    users: Mapped[List[User]] = relationship(secondary=association_table, back_populates="chat")
-    messages: Mapped[List["Messages"]] = relationship(back_populates="chat")
+    users: Mapped[List[User]] = relationship(secondary=association_table, back_populates="chats")
+    messages: Mapped[Optional[List["Messages"]]] = relationship(back_populates="chat", cascade="all, delete-orphan")
+
+    def __repr__(self) -> str:
+        return f"<Chats id={self.id}>"
 
 class Messages(db.Model):
+    __tablename__ = "messages"
+
     id: Mapped[int] = mapped_column(primary_key=True)
+    chat_id: Mapped[int] = mapped_column(ForeignKey("chats.id"))
     chat: Mapped[Chats] = relationship(back_populates="messages")
+    timestamp: Mapped[datetime] = mapped_column(default=datetime.utcnow)
+    text: Mapped[str] = mapped_column(ForeignKey("user.id"))
+    author_id: Mapped[int]
+
+    author: Mapped[User] = relationship()
+
+    def __repr__(self) -> str:
+        return f"<Messages id={self.id} timestamp={self.timestamp} text={self.text} author-username={self.author.username}>"
+
